70524fe605639a99762f4f9299889f78f1fc58cf
vradu007 <radu.voin@gmail.com>
2021-02-10 23:30:55 +0200

    added a background image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import styled from "styled-components";
 
+import backgroundImage from "./assets/background-image.jpg";
 import DetailsPage from "./pages/DetailsPage";
 import MainPage from "./pages/MainPage";
 
+const MainWrapper = styled.div`
+	background: url(${backgroundImage});
+    height: 100%;
+    width: 100%;
+    display: block;
+    position: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+`;
+
 const App = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route path="/" exact component={MainPage} />
-				<Route path="/pokemon/details" exact component={DetailsPage} />
-				<Redirect to="/" />
-			</Switch>
-		</Router>
+		<MainWrapper>
+			<Router>
+				<Switch>
+					<Route path="/" exact component={MainPage} />
+					<Route
+						path="/pokemon/details"
+						exact
+						component={DetailsPage}
+					/>
+					<Redirect to="/" />
+				</Switch>
+			</Router>
+		</MainWrapper>
 	);
 };
 
