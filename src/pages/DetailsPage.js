856588d8749a04d81cdad744b60d0dddd3c4c548
vradu007 <radu.voin@gmail.com>
2021-02-13 17:56:31 +0200

    fetched data for details page as well

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,26 +1,24 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 import Header from "../components/Header";
 
-const getPokemonById = (pokemonId) => {
-    let pokemon = {};
-    try {
-        pokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-            .then((response) => response.json());
-    } catch (error) {
-        console.log(error);
-    }
-    return pokemon;
-};
-
 const DetailsPage = (props) => {
-    const pokemonId = props.match.params.pokemonId;
-    const pokemon = getPokemonById(pokemonId);
-
-    return(
-        <h2></h2>
-    );
+	const pokemonId = props.match.params.pokemonId;
+	const [pokemon, setPokemon] = useState({});
+	const getPokemonById = async (pokemonId) => {
+		let pokemon = {};
+		try {
+			pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+				.then((response) => response.json());
+		} catch (error) {
+			console.log(error);
+		}
+		return pokemon;
+	};
+	useEffect(async () => {
+		setPokemon(await getPokemonById(pokemonId));
+	}, []);
+	return <Header title={`Details page for ${pokemon.name}`} />;
 };
 
 export default DetailsPage;
